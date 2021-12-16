3d1986981362ba49e882139322472d3c37d7522f
PaolaUcreativa <maria.rojas.valverde@ucreativa.com>
2021-12-16 12:13:57 -0600

    Tarea04

--- src/test/java/apiTest/DummyRestApiExample.java
+++ src/test/java/apiTest/DummyRestApiExample.java
@@ -1,21 +1,27 @@
 package apiTest;
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
+import dataEntities.Employee;
+import helpers.dataHelper;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
-import io.restassured.specification.*;
-import org.testng.annotations.*;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class DummyRestApiExample {
     RequestSpecification requestSpecification;
     ResponseSpecification responseSpecification;
+    Employee employee;
 
     @BeforeTest
     public void setUp(){
         requestSpecification = new RequestSpecBuilder().
-                setBaseUri("http://dummy.restapiexample.com/api/v1").build();
+                setBaseUri("http://dummy.restapiexample.com/api/v1").setContentType(ContentType.JSON).build();
         responseSpecification = new ResponseSpecBuilder().
                 expectStatusCode(200).
                 expectContentType(ContentType.JSON).
@@ -29,44 +35,61 @@ public class DummyRestApiExample {
                 when().get("http://dummy.restapiexample.com/api/v1/employees").
                 then().assertThat().
                 statusCode(200).log().body();*/
-
-
         //refactor to use specifications
-        /*given().spec(requestSpecification).
+        given().spec(requestSpecification).
                 get("employees").
                 then().spec(responseSpecification).
-                log().body();*/
+                log().body();
     }
 
     @Test
     public void GetOneEmpReturnsCorrectDataAndStatusCode200(){
         //initial structure before refactor with specifications
-       /* given().
+        /*given().
                 when().get("http://dummy.restapiexample.com/api/v1/employee/24").
                 then().assertThat().
                 body("data.employee_name", equalTo("Doris Wilder")).
                 and().
                 statusCode(200).
                 log().body();*/
-
         //refactor to use specifications
-        /*given().
+        given().
                 spec(requestSpecification).
                 get("employee/24").
                 then().
                 spec(responseSpecification).
                 assertThat().body("data.employee_name", equalTo("Doris Wilder")).
-                log().body();*/
+                log().body();
     }
-    @Test
-    public void DeleteOneEmployee(){
 
+    @Test
+    public void CreateOneEmployee(){
+        initEmployee();
+        given().
+                spec(requestSpecification.body(employee)).
+                post("create").then().spec(responseSpecification).log().body();
+    }
+
+    @Test
+    public void UpdateOneEmployee(){
+        initEmployee();
+        given().
+                spec(requestSpecification.body(employee)).
+                put("update/1").then().spec(responseSpecification).log().body();
+    }
+
+    private void initEmployee(){
+        employee = new Employee(dataHelper.generateName(), dataHelper.generateRandomSalary(), dataHelper.generateRandomAge());
+    }
+
+    @Test
+    public void DeleteEmployeeFromRecord(){
         given().
                 spec(requestSpecification).delete("delete/2").
                 then().
                 spec(responseSpecification).
-                assertThat().body("message", equalTo("Successfully! Record has been deleted")).
-                log().body();
+                assertThat().body("message", equalTo("Successfully! Record has been deleted")).log().body();
     }
+
 
 }
